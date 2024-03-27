13d23ded35422ab383f5768f6fd7096c062b13a0
David Castillo <casvel.d@gmail.com>
2024-03-26 22:47:34 -0700

    Agregar Actividad 1

---
+++ Actividad 1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Bienvenidos a UMI/Universidad Coppel");
+
+//definir variables
+
+string nombre;
+string edad;
+string fecha_de_nacimiento;
+string carrera;
+
+Console.WriteLine("\nIngrese nombre completo");
+nombre= Console.ReadLine();
+
+Console.WriteLine("\nEdad");
+edad = Console.ReadLine();
+
+Console.WriteLine("\nFecha de naciminento");
+fecha_de_nacimiento= Console.ReadLine();
+
+Console.WriteLine("\nCarrera");
+carrera = Console.ReadLine();
+
+Console.WriteLine("\nGracias " + nombre +" por formar parte de UMI/Universidad Coppel, bienvenido a la carrera " + carrera);
+
+Console.WriteLine("\nNombre: " + nombre);
+Console.WriteLine("\nEdad: " + edad);
+Console.WriteLine("\nFecha de nacimiento: " + fecha_de_nacimiento);
+Console.WriteLine("\nCarrera: " +  carrera);
+
+
+
+   
+
+
+
+
